16247432ae0e034307474bf941236dea9c42aa44
Jose DeLavalle <me@slickwebstudio.com>
2016-11-16 18:39:51 -0500

    cleaner code

--- server.js
+++ server.js
@@ -16,23 +16,25 @@ router.use(function(req, res, next) {
 
 router.get('/', function(req, res) {
     var url = "http://www.michaelkors.com/women/clothing/_/N-28ef";
-    var tmpObj = {};
     var myObj = [];
     request(url, function(err, resp, body) {
         if(!err && resp.statusCode == 200) {
             var $ = cheerio.load(body);
+            //LOOK THROUGH DOM FOR SPECIFIC ELEMENTS
             $('.productPanelMain','#main_container').each(function() {
-                tmpObj.image = ($(this).find('img').attr('src'));
-                tmpObj.product = ($(this).find('.prod_name h6').text());
-                tmpObj.category = ($(this).find('.prod_name span').text());
-                tmpObj.price = ($(this).find('div.product_description').text()  );
-                myObj.push(tmpObj);
-            });
-            res.send(myObj);
+                var row = {
+                    "image" : ($(this).find('img').attr('src')),
+                    "product" : ($(this).find('.prod_name h6').text()),
+                    "price" : ($(this).find('div.product_description').text())
+                };
+                
+                myObj.push(row);
+            });       
             console.log('data sent');
         } else {
             console.log(err);
         }
+        res.json(myObj);
     });
     
 });
